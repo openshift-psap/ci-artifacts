f7f23ec2093612696e02b80433c6620f6d2bb80d
Kevin Pouget <kpouget@redhat.com>
2021-04-27 10:30:09 +0200

    refactor matrix_mult role into gpu_operator_run_matrix-mult

--- toolbox/gpu-operator/run_matrix_mult.sh
+++ toolbox/gpu-operator/run_matrix_mult.sh
@@ -3,17 +3,13 @@
 THIS_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 source ${THIS_DIR}/../_common.sh
 
-
 if [ "$#" -gt 1 ]; then
     echo "FATAL: expected 0 or 1 parameter ... (got $#: '$@')"
-#   echo "Usage: $0 [gpu-burn runtime]"
     echo "Usage: $0 [matrix-mult GPU]"
     exit 1
 elif [ "$#" -eq 1 ]; then
-#   ANSIBLE_OPTS="${ANSIBLE_OPTS} -e gpu_burn_time=$1"
     ANSIBLE_OPTS="${ANSIBLE_OPTS} -e matrix_mult_time=$1"
-    echo "Running GPU Matrix Mult for ${1} seconds."
+    echo "Running MLPerf Matrix Multiply for ${1} seconds."
 fi
 
-#exec ansible-playbook ${ANSIBLE_OPTS} playbooks/gpu-burn.yml
-exec ansible-playbook ${ANSIBLE_OPTS} playbooks/matrix-mult.yml
+exec ansible-playbook ${ANSIBLE_OPTS} playbooks/gpu_operator_run_matrix-mult.yml
