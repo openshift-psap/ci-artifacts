284cfc14357cd4a6d91ff1da00499ce43a547a90
Kevin Pouget <kpouget@redhat.com>
2021-06-24 10:45:21 +0200

    entitled-mirror: deploy: create ascii CM instead of binary

--- subprojects/entitled-mirror/deploy.sh
+++ subprojects/entitled-mirror/deploy.sh
@@ -11,11 +11,14 @@ if ! oc get nodes -lentitled=true -oname; then
     exit 1
 fi
 
+oc delete --ignore-not-found=true cm ${NAME}-sync-script ${NAME}-nginx
+
+oc create -n ${NAMESPACE} cm ${NAME}-sync-script --from-file=./containers/sync/sync_commands.sh
+oc create -n ${NAMESPACE} cm ${NAME}-nginx       --from-file=./containers/serve/nginx.conf
+
 oc process -f ${SCRIPT_DIR}/openshift/template.yaml \
     -p NAME=${NAME} \
     -p NAMESPACE=${NAMESPACE} \
     -p QUAY_REPO=${QUAY_REPO} \
     -p "CLIENT_CA=$( < ./auth/ca/generated_ca.crt base64 --wrap=0 )" \
-    -p "NGINX_CONFIG=$( < ./containers/serve/nginx.conf base64 --wrap=0 )" \
-    -p "SYNC_COMMANDS=$( < ./containers/sync/sync_commands.sh base64 --wrap=0 )" \
     | oc apply -f -
