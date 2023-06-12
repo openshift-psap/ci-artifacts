b1bf145006a8bc1a0828f05c3d56083695674f09
David Gray <dagray@redhat.com>
2023-06-12 16:44:49 -0400

    Update testing/model-mesh scripts and manifests

--- testing/model-mesh/4-deploy-many-ns-many-models.sh
+++ testing/model-mesh/4-deploy-many-ns-many-models.sh
@@ -15,6 +15,8 @@ for i in $(seq 1 ${NS_COUNT})
 do
     NS=${NS_BASENAME}-${i}
     oc create ns ${NS}
+    oc label namespace ${NS} modelmesh-enabled=true --overwrite=true
+    oc label namespace ${NS} opendatahub.io/dashboard=true --overwrite=true
     oc apply -f ${THIS_DIR}/minio-secret.yaml -n ${NS}
 done
 unset NS
@@ -26,7 +28,9 @@ do
     for i in $(seq 1 ${NS_COUNT})
     do
         NS=${NS_BASENAME}-${i}
-        sed s/example-onnx-mnist/example-onnx-mnist-${j}/g ${THIS_DIR}/triton.yaml | oc apply -n ${NS} -f -
+        oc apply -n ${NS} -f ${THIS_DIR}/servingruntime.yaml
+        sed s/example-onnx-mnist/example-onnx-mnist-${j}/g ${THIS_DIR}/inferenceservice.yaml | oc apply -n ${NS} -f -
+        oc apply -f  ${THIS_DIR}/sa_user.yaml -n ${NS}
     done
 done
 unset NS
@@ -36,7 +40,7 @@ for i in $(seq 1 ${NS_COUNT})
 do
     NS=${NS_BASENAME}-${i}
 
-    until [[ "$(oc get pods -n ${NS} | grep '4/4' |grep Running |wc -l)" == ${MM_POD_COUNT} ]]
+    until [[ "$(oc get pods -n ${NS} | grep '5/5' |grep Running |wc -l)" == ${MM_POD_COUNT} ]]
     do
         echo "NS:${NS}: Waiting for the model mesh pods"
         sleep 1
@@ -51,11 +55,12 @@ do
     if [[ "$API_ENDPOINT_CHECK" -eq 0 ]]
     then
 	NS=${NS_BASENAME}-${i}
-        route=$(oc -n ${NS} get routes example-onnx-mnist-$i --template={{.spec.host}}{{.spec.path}})
+        route=$(oc -n ${NS} get routes mm-${i}ovms-1.x-model-route --template={{.spec.host}})
+        INFERENCE_TOKEN=$(oc create token user-one -n ${NS})
         for i in $(seq 1 ${MODEL_COUNT})
         do
             echo "NS:${NS}: Smoke-testing endpoint example-onnx-mnist-$i"
-            until curl $CURL_OPTIONS https://${route}/infer -d @${THIS_DIR}/input-onnx.json | jq '.outputs[] | select(.data != null)' &>/dev/null
+            until curl -H "Authorization: Bearer ${INFERENCE_TOKEN}" $CURL_OPTIONS https://${route}/v2/models/example-onnx-mnist-$i/infer -d @${THIS_DIR}/input-onnx.json | jq '.outputs[] | select(.data != null)' &>/dev/null
             do
                 echo "S:${NS}: Waiting for inference endpoint example-onnx-mnist-$i"
                 sleep 1
